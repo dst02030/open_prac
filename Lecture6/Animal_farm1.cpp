89df55a9aa909aed20329506972ea66e27202f94
HinJ <21600695@handong.edu>
2017-11-24 09:49:14 +0000

    complete_farm1

--- Lecture6/Animal_farm1.cpp
+++ Lecture6/Animal_farm1.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 
-void Dog_crt(){
+
+void Dog_cry(){
     std::cout<<"Mung-mung! \n";
 };
 
 void Cat_cry(){
     std::cout<<"Ya-ong! \n";
 };
+int main(){
+    Dog_cry();
+    Cat_cry();
+}
