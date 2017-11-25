9d854e93ff5c239a7b97e47a095a9571a17267c4
HinJ <21600695@handong.edu>
2017-11-25 13:13:26 +0000

    reference

---
+++ Lecture7/&Swap.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+int swap (int &a, int&b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int main(){
+    int i=3;
+    int j=2;
+    
+    std::cout<<"i= " << i << "\nj= " << j<<std::endl;
+    
+    swap(i,j);
+    
+    int &a = i; 
+    int &b = j;
+    
+    std::cout<<"i= " << i << "\nj= " << j<<std::endl;
+    
+}
