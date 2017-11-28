4d0ae2d81e466415e80f4f8f0dfaa817a77ca91c
HinJ <21600695@handong.edu>
2017-11-28 18:50:21 +0000

    constructor2

--- Lecture/Lecture8/record.cpp
+++ Lecture/Lecture8/record.cpp
@@ -10,10 +10,26 @@ class record{
     private:
     int course1, course2;
     double avg;
+    
     public:
     record();
+    record(char*, int);
+    record(char*, int, int);
     void print(void);
 };
+
+record::record(char* str, int score){
+    strcpy(name, str);
+    course1 = course2= score;
+    avg = score;
+}
+
+record::record(char *str, int score1, int score2){
+    strcpy(name, str);
+    course1 = score1;
+    course2 = score2;
+    avg = ((double)(course1+course2))/2.0;
+}
 
 record::record(){
     strcpy(name, "");
@@ -29,9 +45,9 @@ int main(){
    // record yourRecord = {"Hong GD", 100, 100};    사용하지 않는 방법!! private 속이기에.
    // yourRecord.print();
     
-    record myRecord = record();
-    record hisRecord = record(); // 명시적으로 기본생성자를 호출
-    record herRecord; // 기본 생성자가 호출
+    record myRecord;
+    record hisRecord = record("Kim",80,100); // 명시적으로 기본생성자를 호출
+    record herRecord("Lee",70); // 기본 생성자가 호출
     
     myRecord.print();
     hisRecord.print();
