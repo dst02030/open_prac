a462c83afa00585ffec81a6689cd3897b41d04d1
HinJ <21600695@handong.edu>
2017-11-28 10:04:03 +0000

    Lec7_clear

--- Lecture/Lecture7/CopyConst.cpp
+++ Lecture/Lecture7/CopyConst.cpp
@@ -4,6 +4,22 @@ class Table{
     Table &operator=(const Table&); //copy assignment
 };
 
+Table::Table(const Table &t){
+    p=new Name[sz=t.sz];
+    for (int I=0; I<sz; i++)p[I]=t.p[I];
+}
+
+Table &Table::operator=(const Table& t) // assignment
+{
+    if(this != &t){
+        //beware of self-assignment: t=t
+        delete[] p;
+        p = new Name[sz=t.sz];
+        for(int i=0; i<sz; i++) p[i]=t.p[i];
+    }
+    return *this;
+}
+
 void h(){
     Table t1;
     Table t2 = t1; //copy initialization : trouble 
