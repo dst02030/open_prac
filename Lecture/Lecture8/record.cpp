aa75f948403b3a91551d4614fcbf5e9e28e9ea38
HinJ <21600695@handong.edu>
2017-11-28 18:46:50 +0000

    constructor

--- Lecture/Lecture8/record.cpp
+++ Lecture/Lecture8/record.cpp
@@ -1,3 +1,9 @@
+#include <iostream>
+using namespace std;
+#define MAX 10
+#include <string>
+#include <string.h>
+
 class record{
     private:
     char name[MAX];
@@ -5,10 +11,32 @@ class record{
     int course1, course2;
     double avg;
     public:
-    record(){
-        strcpy(name, ""); //name에 ""를 복사
-        course1 = course2 = 100;
-        avg = 100;
-    }
+    record();
     void print(void);
+};
+
+record::record(){
+    strcpy(name, "");
+    course1=course2=100;
+    avg=100;
+}
+
+record::print(){
+    cout<<"my name is "<<name<<"course1 and 2 are "<< course1<< ", "<<course2<<endl;
+}
+
+int main(){
+   // record yourRecord = {"Hong GD", 100, 100};    사용하지 않는 방법!! private 속이기에.
+   // yourRecord.print();
+    
+    record myRecord = record();
+    record hisRecord = record(); // 명시적으로 기본생성자를 호출
+    record herRecord; // 기본 생성자가 호출
+    
+    myRecord.print();
+    hisRecord.print();
+    herRecord.print();
+
+    
+    return 0;
 }
