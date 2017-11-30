c48f0b0200e508fda094741a1b1f6c7126a27814
HinJ <21600695@handong.edu>
2017-11-30 13:24:49 +0000

    inherit2

--- Lecture/Lecture9/inherit2.cpp
+++ Lecture/Lecture9/inherit2.cpp
@@ -1,4 +1,26 @@
+#include <iostream>
+
 class Point{
+    public:
     int x;
     int y;
+};
+
+class Circle{
+    public:
+    Point center;
+    int radius;
+};
+
+class Cylinder{
+    public:
+    Circle center;
+    int height;
+};
+
+int main(){
+    Cylinder a;
+    a.center.center.x = 3;
+    a.center.radius = 8;
+    a.height = 9;
 }
