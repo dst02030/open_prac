eb502f7924eb1998bb98a1ca51362ffcf35c6f31
HinJ <21600695@handong.edu>
2017-12-02 14:10:42 +0000

    c++_struct2

--- C++_blog/6/c_struct.cpp
+++ C++_blog/6/c_struct.cpp
@@ -1,15 +1,21 @@
-#include <stdio.h>
+#include <iostream>
+
+using namespace std;
 
 struct student {
     int id;
     char *name;
     float percentage;
+    
+    void Show(){
+        cout << "아이디: "<< id<< endl;
+        cout << "이름: "<< name << endl;
+        cout << "백분율: "<< percentage << endl;
+    }
 }; // 구조체 뒤에 세미콜론이 와야함
 
 int main(){
     struct student s = {1, "김철수", 90.5};
-    printf("아이디: %d \n", s.id);
-    printf("이름: %s \n", s.name);
-    printf("백분율: %f \n", s.percentage);
+    s.Show();
     return 0;
 }
