0002bf108091a5bf2d8727c4cf96e4915cde8808
HinJ <21600695@handong.edu>
2017-12-02 13:01:20 +0000

    FuncOver

---
+++ C++_blog/4/FunctionOverloading.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void func(int a)
+{
+    cout << "void func(int a)이 호출됨!" << endl;
+}
+
+void func (int a, int b)
+{
+    cout << "void func(int a, int b)이 호출됨!" << endl;
+}
+
+int main()
+{
+    func(4);
+    func(5,6);
+    
+    return 0;
+}
