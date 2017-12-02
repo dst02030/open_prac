3672576abaf406cbfd7f7547593161119ca3d3cb
HinJ <21600695@handong.edu>
2017-12-02 08:40:29 +0000

    cin_FIN

--- C++ 강좌/2/cin(console_input).cpp
+++ C++ 강좌/2/cin(console_input).cpp
@@ -3,9 +3,14 @@
 int main()
 {
     char name[10];
+    int age;
     
+    std::cin >> name >> age;
+    /*
     std::cin >> name;
-    std::cout << "당신의 이름은 "<< name << "입니다." << std::endl;
+    std::cin >> age;  와 같은 문법.
+    */
+    std::cout << "당신의 이름은 "<< name << "이며, 나이는 " << age << "살입니다." << std::endl;
     
     return 0;
 }
