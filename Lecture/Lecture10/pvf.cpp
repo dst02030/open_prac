597d9560f8220df0f5ed550030578b208beae8c1
HinJ <21600695@handong.edu>
2017-12-05 03:25:10 +0000

    PVF

---
+++ Lecture/Lecture10/pvf.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class Parent{
+    public:
+    virtual void print() = 0;
+};
+
+class Child : public Parent{
+    public:
+    void print(){
+        cout<< "I'm your son." << endl;
+    }
+};
