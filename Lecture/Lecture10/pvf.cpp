7d30858fdb865cb79ede2b421a06767d7b9c7b46
HinJ <21600695@handong.edu>
2017-12-05 03:28:08 +0000

    PVF2

--- Lecture/Lecture10/pvf.cpp
+++ Lecture/Lecture10/pvf.cpp
@@ -12,3 +12,15 @@ class Child : public Parent{
         cout<< "I'm your son." << endl;
     }
 };
+
+int main(){
+    /*
+    Parent parent;
+    parent.print();
+    */
+    
+    Child child;
+    child.print();
+    //child.Parent::print();
+    return 0;
+}
