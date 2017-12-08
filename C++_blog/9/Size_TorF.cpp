ec94aa15a47be129236f4aaad2834bd3bc5b841e
HinJ <21600695@handong.edu>
2017-12-08 09:04:16 +0000

    Test_Bool

---
+++ C++_blog/9/Size_TorF.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "sizeof True: " << sizeof true << endl;
+    
+    cout << "sizeof false: " << sizeof false << endl;
+    
+    return 0;
+    
+}
