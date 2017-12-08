63c9e64849bb88a51b56eb475996c2c0dcf0db89
HinJ <21600695@handong.edu>
2017-12-08 16:41:07 +0000

    friend

---
+++ C++_blog(blog.eairship.kr)/11:Friend/friend.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+class B; // B class의 존재를 알림!
+
+class A{
+    private:
+    int adata;
+    public:
+    A(int _data){
+        adata = _data;
+    }
+    void ShowData(B b);
+    friend class B;
+};
+
+class B{
+    private:
+    int bdata;
+    public:
+    B(int _data){
+        bdata = _data;
+    }
+    void ShowData(A a){
+        cout << "a.data: " << a.adata << endl;
+    }
+    friend class A;
+};
+
+void A::ShowData(B b)
+{
+    cout << "b.data: " << b.bdata << endl;
+}
+
+int main(){
+    A a(10);
+    B b(20);
+    
+    a.ShowData(b);
+    b.ShowData(a);
+    return 0;
+}
