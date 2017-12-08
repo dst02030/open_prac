30c32dd6d6e905002eab2334c5b3da217802694e
HinJ <21600695@handong.edu>
2017-12-08 05:16:29 +0000

    problem of shallow

---
+++ C++_blog/8/Problem_of_shallow.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+class MyClass
+{
+    private:
+    char *str;
+    
+    public:
+    MyClass(const char *aStr)
+    {
+        str = new char[strlen(aStr)+1];
+        strcpy(str, aStr);
+    }
+    ~MyClass(){
+        delete []str;
+        cout << "MyClass() called!" << endl;
+    }
+    
+    void ShowData()
+    {
+        cout << "str: " << str << end;
+    }
+};
+
+int main(){
+    MyClass mc1("MyClass!");
+    MyClass mc2 = mc1;
+    
+    mc1.ShowData();
+    mc2.ShowData();
+    return 0;
+}
