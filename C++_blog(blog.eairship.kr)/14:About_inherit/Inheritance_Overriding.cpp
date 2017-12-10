8f55fed49d4654c2fe49ab3d5e1f07b60af43deb
HinJ <21600695@handong.edu>
2017-12-10 06:47:45 +0000

    Inherit_overriding

---
+++ C++_blog(blog.eairship.kr)/14:About_inherit/Inheritance_Overriding.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+class A{
+    public:
+    void over() {
+        cout << "A 클래스의 over 함수 호출!" << endl;
+    }
+};
+
+class B : pubic A {
+    public:
+    void over(){
+        cout << "B 클래스의 over 함수 호출!" << endl;
+    }
+};
+
+int main(){
+    B b;
+    b.over();
+    return 0;
+}
