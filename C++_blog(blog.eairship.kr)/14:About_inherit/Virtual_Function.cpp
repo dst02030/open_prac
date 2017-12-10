6169566211158b98aa29b231ff759a83ecdcbf36
HinJ <21600695@handong.edu>
2017-12-10 07:00:44 +0000

    after_virtual

--- C++_blog(blog.eairship.kr)/14:About_inherit/Virtual_Function.cpp
+++ C++_blog(blog.eairship.kr)/14:About_inherit/Virtual_Function.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class Parent{
     
     public:
-    void func() { cout<<"부모 클래스의 func 함수 호출!" << endl;
+    virtual void func() { cout<<"부모 클래스의 func 함수 호출!" << endl;
     }
     
     
@@ -13,7 +13,7 @@ class Parent{
 
 class Child : public Parent {
     public:
-    void func() { cout << "자식 클래스의 func 함수 호출!" << endl;
+    virtual void func() { cout << "자식 클래스의 func 함수 호출!" << endl;
     }
 };
 
@@ -28,3 +28,10 @@ int main()
     pP->func();
     return 0;
 }
+
+/*
+C++ 컴파일러가 실제로 가리키는 객체의 자료형이 아니라, 포인터
+변수의 자료형을 기준으로 판단한다. 따라서 실제로 가리키는
+객체의 자료형에 따라 멤버 함수가 호출되게 하려면,
+virtual 함수를 사용해야함!
+*/
